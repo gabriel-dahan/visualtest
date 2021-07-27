44276078a60f2572fa90839ce3c0abbcc8a25da9
Gabriel Dahan <gabrieldahan05@gmail.com>
2021-07-27 11:00:09 +0200

    Added setup.py

---
+++ setup.py
@@ -0,0 +1,13 @@
+import setuptools
+
+setuptools.setup(
+    name = "visualtest",
+    version = "1.0",
+    author = "Gabriel D.",
+    description = "A module allowing you to test your functions directly in the shell.",
+    url = "https://github.com/gabriel-dahan/visualtest",
+    packages = setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3"
+    ]
+)
